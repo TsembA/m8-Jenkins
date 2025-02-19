041a2f7ff297e5002a420142ca3c57a0203e3762
Tsemb <Tsembenhoi@gmail.com>
2025-02-19 20:44:50 +0200

    call library functions in JSL

--- java-maven-app/script.groovy
+++ java-maven-app/script.groovy
@@ -1,17 +1,3 @@
-def buildJar() {
-    echo 'building the application...'
-    sh 'mvn package'
-}
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t dancedevops/java-maven-app:jma-6.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push dancedevops/java-maven-app:jma-6.0'
-    }
-}
-
 def deployApp() {
     echo 'deploying the application...'
 }
