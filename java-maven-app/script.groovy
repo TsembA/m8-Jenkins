96c1a903b62ada25b43a81db8917d4a46e66f53b
Tsemb <82046636+TsembA@users.noreply.github.com>
2025-02-19 21:05:31 +0200

    Update script.groovy

--- java-maven-app/script.groovy
+++ java-maven-app/script.groovy
@@ -1,17 +1,3 @@
-def buildJar() {
-    echo 'building the application...'
-    sh 'mvn package'
-}
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t dancedevops/my-app:jma-6.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push dancedevops/my-app:jma-6.0'
-    }
-}
-
 def deployApp() {
     echo 'deploying the application...'
 }
